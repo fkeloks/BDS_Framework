95e642e95080ffc91d547e305dcd7680467eccb0
fkeloks <fkeloks@gmail.com>
2017-07-08 12:46:48 +0200

    Correction tests unitaires sur la classe database()

--- specs/framework/databaseSpec.php
+++ specs/framework/databaseSpec.php
@@ -7,19 +7,19 @@ describe('Database', function () {
             return new \BDSCore\Database\Database();
         })->toThrow(new \BDSCore\Database\DatabaseException);
         expect(function () {
-            return new \BDSCore\Database\Database('databaseTest', 'd');
+            return new \BDSCore\Database\Database('d', 'databaseTest');
         })->toThrow(new \BDSCore\Database\DatabaseException);
-        expect(new \BDSCore\Database\Database('databaseTest', 'sqlite'))->toBeAnInstanceOf('BDSCore\Database\Database');
+        expect(new \BDSCore\Database\Database('sqlite', 'databaseTest'))->toBeAnInstanceOf('BDSCore\Database\Database');
     });
 
     it('Test sending data to a sqlite database.', function () {
-        $database = new \BDSCore\Database\Database('databaseTest', 'sqlite');
+        $database = new \BDSCore\Database\Database('sqlite', 'databaseTest');
         expect($database->query('CREATE TABLE IF NOT EXISTS users ( id INT PRIMARY KEY, name VARCHAR(255), password VARCHAR(255) ); CREATE UNIQUE INDEX users_id_uindex ON users (id); '))->toBeAnInstanceOf('PDOStatement');
         expect($database->query('INSERT INTO users (name, password) VALUES ("Babar", "myPassword")'))->toBeAnInstanceOf('PDOStatement');
     });
 
     it('Testing recovery of data from a sqlite database.', function () {
-        $database = new \BDSCore\Database\Database('databaseTest', 'sqlite');
+        $database = new \BDSCore\Database\Database('sqlite', 'databaseTest');
         expect($database->fetch('SELECT * FROM users'))->not->toBeFalsy();
         expect($database->fetchAll('SELECT * FROM users'))->toBeA('array');
         expect($database->fetchColumn('SELECT name, password FROM users'))->not->toBeFalsy();
